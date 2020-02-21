20a6762e3259fa93d8abc83c84833cb9aa49a239
Alberto Navarrete Garcia <alberto.navarrete@axity.com>
2020-02-21 00:25:05 -0600

    Base inicial.

---
+++ backend/j-order/j-order-war/src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>J Order - Microservice reference implementation.</h2>
+</body>
+</html>
---
+++ backend/j-order/microservice-base/src/main/java/com/archetype/ti/hiworld/app/DefaultApplicationStarter.java
@@ -0,0 +1,17 @@
+package com.archetype.ti.hiworld.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class DefaultApplicationStarter extends SpringBootServletInitializer {
+	@Override
+	   protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	      return application.sources(DefaultApplicationStarter.class);
+	   }
+	   public static void main(String[] args) {
+	      SpringApplication.run(DefaultApplicationStarter.class, args);
+	   }
+}
